2d017e93210cbddf3bb4d1d23c3076eaddcbe8b8
05126619z <94692551+05126619z@users.noreply.github.com>
2026-03-02 16:18:35 -0300

    Initial commit

---
+++ ScavTemplate/Patches.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace ModNamespace
+{
+    internal class Patches
+    {
+        [HarmonyPatch(typeof(ConsoleScript))]
+        internal static class ConsolePatch
+        {
+            [HarmonyPatch(nameof(ConsoleScript.Start))]
+            [HarmonyPostfix]
+            private static void StartPatch()
+            {
+                ConsoleScript.instance.LogToConsole("Hello World!");
+            }
+        }
+    }
+}
---
+++ ScavTemplate/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using MonoMod.RuntimeDetour;
+using UnityEngine;
+
+namespace ModNamespace
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string ModGUID = "05126619z.template";
+        public const string ModName = "Template";
+        public const string ModVersion = "0.0.0";
+
+        internal static new ManualLogSource Logger;
+        private readonly Harmony _harmony = new(ModGUID);
+        public static Plugin Instance { get; private set; } = null!;
+
+        public void Awake()
+        {
+            Logger = base.Logger;
+            Instance = this;
+
+            _harmony.PatchAll();
+            Logger.LogInfo($"Plugin ModName is loaded!");
+        }
+    }
+}
